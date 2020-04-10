f1eb3081bf4727cf77040a101ec530929cef5aa1
nicolau.pro <radu.alex.nicolau@gmail.com>
2020-04-10 12:16:33 +0100

    Implemented in react app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import Display from './components/Display';
 
 class App extends Component {
   constructor(props) {
@@ -13,64 +14,21 @@ class App extends Component {
         require('./data/example.json'),
         require('./data/sucess.json'),
       ],
-      currentInput: 5,
     };
   }
 
   componentDidMount() {
-    console.log('Loaded input');
-    console.table(this.state.inputs[this.state.currentInput].data);
-
-    function getProcessingPage(data) {
-      let message = data[0];
-      let state = message.state;
-      let errorCode = message.errorCode;
-
-      console.log(message);
-
-      switch (state) {
-        case 'sucess':
-          console.log("RETURN: { title: 'Order complete', message: null }");
-          return { title: 'Order complete', message: null };
-
-        case 'processing':
-          if (data.length > 1) {
-            data.shift();
-            setTimeout(function () {
-              getProcessingPage(data);
-            }, 200);
-          } else {
-            console.log("RETURN: { title: 'Error page', message: null }");
-            return { title: 'Error page', message: null };
-          }
-          break;
-
-        case 'error':
-          switch (errorCode) {
-            case 'NO_STOCK':
-              console.log("RETURN: { title: 'Error page', message: 'No stock has been found' }");
-              return { title: 'Error page', message: 'No stock has been found' };
-
-            case 'INCORRECT_DETAILS':
-              console.log("RETURN: { title: 'Error page', message: 'Incorrect details have been entered' }");
-              return { title: 'Error page', message: 'Incorrect details have been entered' };
-
-            default:
-              console.log("RETURN: { title: 'Error page', message: null }");
-              return { title: 'Error page', message: null };
-          }
-
-        default:
-          console.log("RETURN: { title: 'Error page', message: null }");
-          return { title: 'Error page', message: null };
-      }
-    }
-
-    getProcessingPage(this.state.inputs[this.state.currentInput].data);
+    console.log('Loaded inputs');
   }
 
   render() {
-    return <></>;
+    return (
+      <>
+        {this.state.inputs.map((input, index) => (
+          <Display key={index} input={input} />
+        ))}
+      </>
+    );
   }
 }
 
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,9 +1,82 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-class Display extends Component {
-  state = {};
+function getProcessingPage(data) {
+  const message = data[0];
+
+  switch (message.state) {
+    case 'sucess':
+      console.log("RETURN: { title: 'Order complete', message: null }");
+      return { title: 'Order complete', message: null };
+
+    case 'processing':
+      console.log('processing' + '.'.repeat(data.length - 1));
+      if (data.length > 1) {
+        let newData = [...data];
+        newData.shift();
+        setTimeout(function () {
+          getProcessingPage(newData);
+        }, 2000);
+      } else {
+        console.log("RETURN: { title: 'Error page', message: null }");
+        return { title: 'Error page', message: null };
+      }
+      break;
+
+    case 'error':
+      switch (message.errorCode) {
+        case 'NO_STOCK':
+          console.log("RETURN: { title: 'Error page', message: 'No stock has been found' }");
+          return { title: 'Error page', message: 'No stock has been found' };
+
+        case 'INCORRECT_DETAILS':
+          console.log("RETURN: { title: 'Error page', message: 'Incorrect details have been entered' }");
+          return { title: 'Error page', message: 'Incorrect details have been entered' };
+
+        default:
+          console.log("RETURN: { title: 'Error page', message: null }");
+          return { title: 'Error page', message: null };
+      }
+
+    default:
+      console.log("RETURN: { title: 'Error page', message: null }");
+      return { title: 'Error page', message: null };
+  }
+}
+
+export class Display extends Component {
+  process = () => {
+    let data = this.props.input.data;
+    console.clear();
+    console.table(data);
+    getProcessingPage(data);
+  };
+
   render() {
-    return <section className="row display">Display</section>;
+    return (
+      <section className='display'>
+        <ol start='0'>
+          {this.props.input.data.map((message, index) => (
+            <li key={index}>
+              <span>
+                state: <strong>{message.state}</strong>
+                {message.errorCode ? (
+                  <>
+                    , errorCode: <strong>{message.errorCode}</strong>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </span>
+            </li>
+          ))}
+        </ol>
+        <button type='button' onClick={this.process}>
+          Process
+        </button>
+        <em>&nbsp;See console for details...</em>
+        <hr />
+      </section>
+    );
   }
 }
 
